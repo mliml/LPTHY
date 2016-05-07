a2a16def1abcc8a90875f546f4c259c7606a62cd
mliml <diiyoung.w@gmail.com>
2016-05-07 16:06:38 +0800

    day3

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Stanley Wu'
+my_age = 22 #it's true
+my_height = 175 #CM
+my_weight = 69 #KG
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d cm tall." % my_height
+print "He's %d kg heavy." % my_weight
+print "Avtually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height+ my_weight)
