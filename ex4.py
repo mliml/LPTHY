89426c34ecad26995f04f5efe8d7f0da3a21783a
mliml <diiyoung.w@gmail.com>
2016-05-07 14:32:31 +0800

    day3

--- ex4.py
+++ ex4.py
@@ -1,10 +1,10 @@
 cars = 100 
 space_in_a_car = 4.0
-driversd = 30
+drivers = 30
 passengers = 90
 cars_not_driven = cars - drivers
-cas_driven = drivers
-carpool_capacity = cars_driven + space_in_a_car
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
 
