292200fa3906f738ac35d66c7c6c3bd02988f574
mliml <diiyoung.w@gmail.com>
2016-05-15 20:34:09 +0800

    day10

---
+++ ex22.py
@@ -0,0 +1,19 @@
+#print 打印
+# = 赋值
+# == 检查是否相等
+# %s 输出字符串
+# %d 输出数字
+# %r 打印测试字符
+# \n 换行
+# \t 水平制表符
+# raw_input() 接收输入
+# from sys import argv 
+# script, first, second = argv 解包，在运行程序的时候输入两个参数
+# """ 三引号，可以把几行的引用起来
+# txt = open(filename) 打开文件，返回一个 file object 文件
+# open（文件名，参数）参数：'w'写入 'r'读取 'a'追加
+# txt.read() 读出文件
+# close 关闭文件
+# readline 读取文本的其中一行
+# truncate 清空文件
+# f.seek(0) 回到文件初识阶段，对应file object 文件
